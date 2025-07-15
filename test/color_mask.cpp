72f61f72531cdae71510db4563211d3203e52295
Erik Holum <erik.holum@nasa.gov>
2025-07-15 11:31:38 -0600

    Add pre-commit and apply fixes

--- test/color_mask.cpp
+++ test/color_mask.cpp
@@ -2,37 +2,33 @@
 
 #include "color_names/ColorNames.h"
 
-int32_t main(int32_t argc, char** argv)
-{
-    char key = 0x00;
-    cv::VideoCapture camera(0);
-    ColorNames colorNames("bgr8");
-    std::string color = "yellow";
-    if(!camera.isOpened())
-    {
-        std::cerr << "ERROR: Could not open camera" << std::endl;
-        return 1;
-    }
-    if(argc > 1)
-    {
-        color = argv[1];
-    }
+int32_t main(int32_t argc, char **argv) {
+  char key = 0x00;
+  cv::VideoCapture camera(0);
+  ColorNames colorNames("bgr8");
+  std::string color = "yellow";
+  if (!camera.isOpened()) {
+    std::cerr << "ERROR: Could not open camera" << std::endl;
+    return 1;
+  }
+  if (argc > 1) {
+    color = argv[1];
+  }
 
-    cv::Mat image;
-    cv::Mat mask;
+  cv::Mat image;
+  cv::Mat mask;
 
-    while(key != 'q')
-    {
-        // show the image on the window
-        camera.read(image);
-        colorNames.createColorMask(image, color, mask);
-        cv::Mat display;
-        cv::cvtColor(image, display, cv::COLOR_BGR2GRAY);
-        cv::cvtColor(display, display, cv::COLOR_GRAY2BGR);
-        image.copyTo(display, mask);
-        cv::imshow("grey", display);
+  while (key != 'q') {
+    // show the image on the window
+    camera.read(image);
+    colorNames.createColorMask(image, color, mask);
+    cv::Mat display;
+    cv::cvtColor(image, display, cv::COLOR_BGR2GRAY);
+    cv::cvtColor(display, display, cv::COLOR_GRAY2BGR);
+    image.copyTo(display, mask);
+    cv::imshow("grey", display);
 
-        key = cv::waitKey(1);
-    }
-    return 0;
+    key = cv::waitKey(1);
+  }
+  return 0;
 }
--- test/version.cpp
+++ test/version.cpp
@@ -2,8 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int32_t main(int32_t, char**)
-{
-    printf("OpenCV version: %s\n", CV_VERSION);
-    return (0);
+int32_t main(int32_t, char **) {
+  printf("OpenCV version: %s\n", CV_VERSION);
+  return (0);
 }
