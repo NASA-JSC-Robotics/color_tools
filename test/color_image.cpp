e78143651d34a18c650bca888a8c3438903e25d7
Erik Holum <erik.holum@nasa.gov>
2025-07-15 11:35:46 -0600

    Add license info

--- test/color_image.cpp
+++ test/color_image.cpp
@@ -1,12 +1,33 @@
+/**
+ * Copyright (c) 2025, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ *
+ * All rights reserved.
+ *
+ * This software is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 #include <opencv2/opencv.hpp>
 
 #include "color_names/ColorNames.h"
 
-int32_t main(int32_t, char **) {
+int32_t main(int32_t, char**)
+{
   char key = 0x00;
   cv::VideoCapture camera(0);
   ColorNames colorNames;
-  if (!camera.isOpened()) {
+  if (!camera.isOpened())
+  {
     std::cerr << "ERROR: Could not open camera" << std::endl;
     return 1;
   }
@@ -15,19 +36,15 @@ int32_t main(int32_t, char **) {
   cv::Mat image;
   cv::Mat colorImage;
 
-  while (key != 'q') {
+  while (key != 'q')
+  {
     // show the image on the window
     camera.read(image);
-    std::chrono::time_point<std::chrono::steady_clock> start =
-        std::chrono::steady_clock::now();
+    std::chrono::time_point<std::chrono::steady_clock> start = std::chrono::steady_clock::now();
     colorNames.colorImage(image, colorImage);
-    std::chrono::time_point<std::chrono::steady_clock> end =
-        std::chrono::steady_clock::now();
+    std::chrono::time_point<std::chrono::steady_clock> end = std::chrono::steady_clock::now();
     std::cout << "Elapsed time in microseconds: "
-              << std::chrono::duration_cast<std::chrono::milliseconds>(end -
-                                                                       start)
-                     .count()
-              << " ms" << std::endl;
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() << " ms" << std::endl;
     cv::imshow("Image", colorImage);
 
     key = cv::waitKey(1);
--- test/color_mask.cpp
+++ test/color_mask.cpp
@@ -1,24 +1,47 @@
+/**
+ * Copyright (c) 2025, United States Government, as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ *
+ * All rights reserved.
+ *
+ * This software is licensed under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 #include <opencv2/opencv.hpp>
 
 #include "color_names/ColorNames.h"
 
-int32_t main(int32_t argc, char **argv) {
+int32_t main(int32_t argc, char** argv)
+{
   char key = 0x00;
   cv::VideoCapture camera(0);
   ColorNames colorNames("bgr8");
   std::string color = "yellow";
-  if (!camera.isOpened()) {
+  if (!camera.isOpened())
+  {
     std::cerr << "ERROR: Could not open camera" << std::endl;
     return 1;
   }
-  if (argc > 1) {
+  if (argc > 1)
+  {
     color = argv[1];
   }
 
   cv::Mat image;
   cv::Mat mask;
 
-  while (key != 'q') {
+  while (key != 'q')
+  {
     // show the image on the window
     camera.read(image);
     colorNames.createColorMask(image, color, mask);
